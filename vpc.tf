f8448ee008054710fa0467ca4e2c51e3afefca9d
bin wang <bwangdal@gmail.com>
2020-09-20 13:58:19 +0000

    Add VPC module2

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,6 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 module "vpc" {
   source  = "app.terraform.io/bwangrmd-training/vpc/aws"
   version = "2.51.0"
+  cidr = "10.0.3.0/24"
   # insert required variables here
 }
